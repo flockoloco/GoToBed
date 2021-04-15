de4b07025f5fb41351507995784dfba75a79708e
Clickba <66256524+Clickba@users.noreply.github.com>
2021-04-15 17:13:59 +0100

    a

---
+++ GoToBedFolder/GoToBedProject/Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float mouseSensitivity = 180f;
+    private float cameraXAxis = 0;
+    private float cameraYAxis = 0;
+    public static void setMouseLock(bool locked)
+    {
+        if (locked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+    private void Start()
+    {
+        setMouseLock(true);
+    }
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            float mouseYInput = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivity;
+            float mouseXInput = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity;
+            cameraXAxis -= mouseYInput;
+            cameraYAxis += mouseXInput;
+            cameraXAxis = Mathf.Clamp(cameraXAxis, -90, 90);
+            transform.localEulerAngles = new Vector3(cameraXAxis, cameraYAxis, 0f);
+        }
+    }
+}
---
+++ GoToBedFolder/GoToBedProject/Assets/Scripts/FiniteStateMachine/_Bases/FiniteStateMachine.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiniteStateMachine : MonoBehaviour
+{
+    public State initialState;
+    private State currentState;
+    private GameObject gameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = initialState;
+        
+    }
+
+    public GameObject GetObject()
+    {
+        return gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transition triggeredTransition = null;
+        foreach (Transition t in currentState.GetTransitions())
+        {
+            if (t.IsTriggered(this))
+            {
+                triggeredTransition = t;
+                break;
+            }
+
+        }
+        List<Action> actions = new List<Action>();
+        if (triggeredTransition)
+        {
+            State targetState = triggeredTransition.GetTargetState();
+            actions.Add(currentState.GetExitAction());
+
+            //add if getaction  not null---
+            actions.Add(triggeredTransition.GetAction());
+            ///----
+            actions.Add(targetState.GetEntryAction());
+            currentState = targetState;
+
+        }
+        else
+        {
+            foreach (Action a in currentState.GetActions())
+            {
+                //add if not null---
+                actions.Add(a);
+                //---
+            }
+        }
+        DoActions(actions);
+    }
+    private void DoActions(List<Action> actions)
+    {
+        foreach (Action a in actions)
+        {
+            if (a != null)
+            {
+                a.Act(this);
+            }
+        }
+    }
+}
